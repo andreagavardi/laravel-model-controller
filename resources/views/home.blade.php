e9a2daba543d463f31c93b2010a37a7462db83e4
andreagavardi <andreagavardi89@gmail.com>
2021-07-12 16:00:20 +0200

    creato layout di base in movies.index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.app')
-
+@section('title','HOME-Laravel-Model-Controller')
 @section('main_content')
 <h1>Home Page</h1>
 
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,6 +1,20 @@
 @extends('layout.app')
 
+@section('title','MOVIES-Laravel-Model-Controller')
+
 @section('main_content')
 <h1>Movies</h1>
+<div class="movies">
+    @foreach($movies as $movie)
+    <div class="movie">
+        <h2>{{$movie->title}}</h2>
+        <h3>Original Title: {{$movie->original_title}}</h3>
+        <span>Nationality: {{$movie->nationality}}</span>
+        <span>Date: {{$movie->date}}</span>
+        <span>{{$movie->vote}}</span>
+    </div>
+    @endforeach
+
+</div>
 
 @endsection
