707f78174dd8e6ee80e9c76402ea6729dafa9cab
andreagavardi <andreagavardi89@gmail.com>
2021-07-12 15:12:31 +0200

    creato model Movie, recuparate istanze da database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
 
     public function index()
     {
+        $movies = Movie::all();
+        dd($movies);
         return view('movies.index');
     }
 }
