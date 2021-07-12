02fe3eabb080cbe34ff15c4cae9522a0e9e19670
andreagavardi <andreagavardi89@gmail.com>
2021-07-12 16:14:38 +0200

    stilizzato layout con sass

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,18 +3,20 @@
 @section('title','MOVIES-Laravel-Model-Controller')
 
 @section('main_content')
-<h1>Movies</h1>
-<div class="movies">
-    @foreach($movies as $movie)
-    <div class="movie">
-        <h2>{{$movie->title}}</h2>
-        <h3>Original Title: {{$movie->original_title}}</h3>
-        <span>Nationality: {{$movie->nationality}}</span>
-        <span>Date: {{$movie->date}}</span>
-        <span>{{$movie->vote}}</span>
-    </div>
-    @endforeach
+<div class="container">
+    <h1>Movies List</h1>
+    <div class="movies">
+        @foreach($movies as $movie)
+        <div class="movie">
+            <h2>{{$movie->title}}</h2>
+            <h5>Original Title: {{$movie->original_title}}</h5>
+            <span> <strong>Nationality:</strong> {{$movie->nationality}}</span>
+            <span><strong>Date: </strong> {{$movie->date}}</span>
+            <span> <strong>Voto: </strong>{{$movie->vote}}</span>
+        </div>
+        @endforeach
 
+    </div>
 </div>
 
 @endsection
